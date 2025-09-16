d887041490037fa9e9028bec949120dbbd9ecaa9
Aisyah Mokhtar <aimokhta@u80z06s02.42kl.edu.my>
2025-09-16 11:41:08 +0800

    fix: ex00: add few cases in main.cpp for learning purposes, ex01: just Makefile

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aimokhta <aimokhta@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 09:40:22 by aimokhta          #+#    #+#             */
-/*   Updated: 2025/09/16 11:13:18 by aimokhta         ###   ########.fr       */
+/*   Updated: 2025/09/16 11:39:46 by aimokhta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,17 +34,20 @@ int main ()
 	delete meta;
 
 	// -----------------------------------------------------
-	// |                   wrong classes                   |
+	// |                   "wrong classes"                 |
 	// -----------------------------------------------------
 
 	std::cout << std::endl;
 	const WrongAnimal *k = new WrongAnimal();
 	const WrongAnimal *l = new WrongCat();
 	// const WrongAnimal *m = new Cat(); // not in the same inheritance, thus cannot compile
+	const WrongAnimal *n(l);  // pointer copy, not copy obj - so no copy constructor message
+	// WrongAnimal *o = l; // cannot copy const pointer into  non-const pointer - compiler blocks it
 
 	std::cout << std::endl;
 	std::cout << k->getType() << " " << std::endl;
 	std::cout << l->getType() << " " << std::endl;
+	std::cout << n->getType() << " " << std::endl;
 	k->makeSound();  // const objects can only call const functions (const put after the parameter)
 	l->makeSound();
 	std::cout << std::endl;
